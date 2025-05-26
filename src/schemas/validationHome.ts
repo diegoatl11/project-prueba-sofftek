8a9eb5b773656a7af846a45f72c81fbd4cd3f492
diegoatl11 <diegoatl1102@gmail.com>
2025-05-26 09:09:18 -0500

    datafur: act 5

--- src/schemas/validationHome.ts
+++ src/schemas/validationHome.ts
@@ -3,10 +3,12 @@ import * as Yup from "yup";
 export const validationSchema = Yup.object({
   docNumber: Yup.string()
     .required("Número de documento es obligatorio")
-    .matches(/^\d+$/, "Solo números permitidos"),
+    .matches(/^\d+$/, "Solo números permitidos")
+    .length(8, "El número de documento debe tener 8 dígitos"),
   phone: Yup.string()
     .required("Celular es obligatorio")
-    .matches(/^\d+$/, "Solo números permitidos"),
+    .matches(/^\d+$/, "Solo números permitidos")
+    .length(9, "El número de documento debe tener 9 dígitos"),
   privacy: Yup.boolean().oneOf(
     [true],
     "Debes aceptar la Política de Privacidad"
